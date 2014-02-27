eff9b5b7837fe7a6c3b3747757500e7dbdb1722a
Aaron Cook <acook@allegromicro.com>
2014-02-27 07:43:32 -0500

    Trying to get xrandr to work

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#A simple script to copy files
+sudo cp xorg.conf.dummy /etc/X11/
+cp .xinitrc ~/
--- startdummyx.sh
+++ startdummyx.sh
@@ -8,23 +8,25 @@ $program
    Starts a dummy xserver
    
 Usage:
-   $program <display> <geometery>
+   $program <display>
+   
 END
 }
 
-if [ $2 ]; then
-   geom="$2"
-else
-   geom="1280x1024"
-   echo "Using default $geom"
-fi
+
+#if [ $2 ]; then
+#   geom="$2"
+#else
+#   geom="1280x1024"
+#   echo "Using default $geom"
+#fi
 
 
 if [ $1 ]; then
    echo "Starting Xdummy display $1 geometery $geom"
    
    ## The only important command in this whole script :)
-   startx -- ./Xdummy $1 -geom geom -config /etc/X11/xorg.conf.dummy
+   startx -- ./Xdummy $1 -config /etc/X11/xorg.conf.dummy +extension RANDR
    
 else
    help; exit 0;
